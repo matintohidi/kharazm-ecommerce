9dc1a446ff86659dfa5f02aafce78026b059dd58
matintohidi <matintovhidi@gmail.com>
2025-05-30 16:51:21 +0330

    refactor: remove ThemeProvider and clean up ClientLayout component

--- src/app/ClientLayout.tsx
+++ src/app/ClientLayout.tsx
@@ -3,7 +3,6 @@ import type React from "react";
 import { usePathname } from "next/navigation";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import { ThemeProvider } from "@/components/theme-provider";
 import { CartProvider } from "@/components/cart-provider";
 import { Toaster } from "@/components/ui/toaster";
 
@@ -16,17 +15,13 @@ export default function ClientLayout({
   const isAuthPage = pathname?.startsWith("/auth");
 
   return (
-    <ThemeProvider attribute="class" defaultTheme="light">
-      <CartProvider>
-        <div className="relative flex min-h-screen flex-col">
-          {!isAuthPage && <Header />}
-          <div className={isAuthPage ? "min-h-screen" : "flex-1"}>
-            {children}
-          </div>
-          {!isAuthPage && <Footer />}
-        </div>
-        <Toaster />
-      </CartProvider>
-    </ThemeProvider>
+    <CartProvider>
+      <div className="relative flex min-h-screen flex-col">
+        {!isAuthPage && <Header />}
+        <div className={isAuthPage ? "min-h-screen" : "flex-1"}>{children}</div>
+        {!isAuthPage && <Footer />}
+      </div>
+      <Toaster />
+    </CartProvider>
   );
 }
